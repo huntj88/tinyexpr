252f03b52cb7435adc1cb438e478ddead22ebb65
James Hunt <huntj88@gmail.com>
2025-12-25 09:31:22 -0600

    remove functions I don't plan on using to reduce size

--- tinyexpr.c
+++ tinyexpr.c
@@ -123,36 +123,36 @@ void te_free(te_expr *n) {
 
 
 static double pi(void) {return 3.14159265358979323846;}
-static double e(void) {return 2.71828182845904523536;}
-static double fac(double a) {/* simplest version of fac */
-    if (a < 0.0)
-        return NAN;
-    if (a > UINT_MAX)
-        return INFINITY;
-    unsigned int ua = (unsigned int)(a);
-    unsigned long int result = 1, i;
-    for (i = 1; i <= ua; i++) {
-        if (i > ULONG_MAX / result)
-            return INFINITY;
-        result *= i;
-    }
-    return (double)result;
-}
-static double ncr(double n, double r) {
-    if (n < 0.0 || r < 0.0 || n < r) return NAN;
-    if (n > UINT_MAX || r > UINT_MAX) return INFINITY;
-    unsigned long int un = (unsigned int)(n), ur = (unsigned int)(r), i;
-    unsigned long int result = 1;
-    if (ur > un / 2) ur = un - ur;
-    for (i = 1; i <= ur; i++) {
-        if (result > ULONG_MAX / (un - ur + i))
-            return INFINITY;
-        result *= un - ur + i;
-        result /= i;
-    }
-    return result;
-}
-static double npr(double n, double r) {return ncr(n, r) * fac(r);}
+// static double e(void) {return 2.71828182845904523536;}
+// static double fac(double a) {/* simplest version of fac */
+//     if (a < 0.0)
+//         return NAN;
+//     if (a > UINT_MAX)
+//         return INFINITY;
+//     unsigned int ua = (unsigned int)(a);
+//     unsigned long int result = 1, i;
+//     for (i = 1; i <= ua; i++) {
+//         if (i > ULONG_MAX / result)
+//             return INFINITY;
+//         result *= i;
+//     }
+//     return (double)result;
+// }
+// static double ncr(double n, double r) {
+//     if (n < 0.0 || r < 0.0 || n < r) return NAN;
+//     if (n > UINT_MAX || r > UINT_MAX) return INFINITY;
+//     unsigned long int un = (unsigned int)(n), ur = (unsigned int)(r), i;
+//     unsigned long int result = 1;
+//     if (ur > un / 2) ur = un - ur;
+//     for (i = 1; i <= ur; i++) {
+//         if (result > ULONG_MAX / (un - ur + i))
+//             return INFINITY;
+//         result *= un - ur + i;
+//         result /= i;
+//     }
+//     return result;
+// }
+// static double npr(double n, double r) {return ncr(n, r) * fac(r);}
 
 #ifdef _MSC_VER
 #pragma function (ceil)
@@ -162,33 +162,33 @@ static double npr(double n, double r) {return ncr(n, r) * fac(r);}
 static const te_variable functions[] = {
     /* must be in alphabetical order */
     {"abs", fabs,     TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"acos", acos,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"asin", asin,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"atan", atan,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"atan2", atan2,  TE_FUNCTION2 | TE_FLAG_PURE, 0},
+    // {"acos", acos,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    // {"asin", asin,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    // {"atan", atan,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    // {"atan2", atan2,  TE_FUNCTION2 | TE_FLAG_PURE, 0},
     {"ceil", ceil,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
     {"cos", cos,      TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"cosh", cosh,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"e", e,          TE_FUNCTION0 | TE_FLAG_PURE, 0},
-    {"exp", exp,      TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"fac", fac,      TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    // {"cosh", cosh,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    // {"e", e,          TE_FUNCTION0 | TE_FLAG_PURE, 0},
+    // {"exp", exp,      TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    // {"fac", fac,      TE_FUNCTION1 | TE_FLAG_PURE, 0},
     {"floor", floor,  TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"ln", log,       TE_FUNCTION1 | TE_FLAG_PURE, 0},
-#ifdef TE_NAT_LOG
-    {"log", log,      TE_FUNCTION1 | TE_FLAG_PURE, 0},
-#else
-    {"log", log10,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
-#endif
-    {"log10", log10,  TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"ncr", ncr,      TE_FUNCTION2 | TE_FLAG_PURE, 0},
-    {"npr", npr,      TE_FUNCTION2 | TE_FLAG_PURE, 0},
+    // {"ln", log,       TE_FUNCTION1 | TE_FLAG_PURE, 0},
+// #ifdef TE_NAT_LOG
+//     {"log", log,      TE_FUNCTION1 | TE_FLAG_PURE, 0},
+// #else
+//     {"log", log10,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
+// #endif
+    // {"log10", log10,  TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    // {"ncr", ncr,      TE_FUNCTION2 | TE_FLAG_PURE, 0},
+    // {"npr", npr,      TE_FUNCTION2 | TE_FLAG_PURE, 0},
     {"pi", pi,        TE_FUNCTION0 | TE_FLAG_PURE, 0},
     {"pow", pow,      TE_FUNCTION2 | TE_FLAG_PURE, 0},
     {"sin", sin,      TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"sinh", sinh,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    // {"sinh", sinh,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
     {"sqrt", sqrt,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
     {"tan", tan,      TE_FUNCTION1 | TE_FLAG_PURE, 0},
-    {"tanh", tanh,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
+    // {"tanh", tanh,    TE_FUNCTION1 | TE_FLAG_PURE, 0},
     {0, 0, 0, 0}
 };
 
